28031d01fa6ace6150db7e5421380a827082e274
Vitor Balbo <101026113+balb-0@users.noreply.github.com>
2022-10-31 12:56:28 -0300

    Initial commit

---
+++ m01/aula001/des002.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    printf("\\a\t=\t\tBeep\n");
+    printf("\\n\t=\t\tNova Linha\n");
+    printf("\\t\t=\t\tTabulação\n");
+    printf("\\\\\t=\t\tBarra\n");
+    printf("%%%%\t=\t\tPorcentagem\n");
+    printf("\\\?\t=\t\tInterrogação\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+
+}
